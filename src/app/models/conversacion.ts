cc6a3efeb1fd30686dbe8f44fb500e5dbc3c080d
cardenalcopia <kirisse85@gmail.com>
2022-07-08 09:01:32 +0200

    subida chat

---
+++ src/app/models/conversacion.ts
@@ -0,0 +1,9 @@
+export class Conversacion {
+    public emisor:number;
+    public mensaje:string;
+    constructor(emisor:number,mensaje:string)
+    {
+        this.emisor=emisor
+        this.mensaje=mensaje
+    }
+}
