60d687549d4d3400f81846f1bb09a2732ad527bd
Andrés Cedeño <andresdalopez92@gmail.com>
2022-07-11 19:15:05 +0200

    mapa implementado

---
+++ src/app/components/loading-mapa/loading-mapa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-mapa',
+  templateUrl: './loading-mapa.component.html',
+  styleUrls: ['./loading-mapa.component.css']
+})
+export class LoadingMapaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,18 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+import Mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+ 
+Mapboxgl.accessToken = 'pk.eyJ1IjoiYW5kcmVzLTI5IiwiYSI6ImNsMjJ6enZiMDA0NzQzanFrN3Z4c3RxejQifQ.dlmE5vkLBhkHDfIokDpSPA';
+
+if( !navigator.geolocation ){
+
+  alert('Navegador no soporta la Geolocation');
+
+  throw new Error('Navegador no soporta el Geolocation');
+
+}
+
 if (environment.production) {
   enableProdMode();
 }
