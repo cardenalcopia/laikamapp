ff0fffeafe43e67662e1a958474176553cf87447
cardenalcopia <kirisse85@gmail.com>
2022-07-20 15:05:41 +0200

    Subida Chat

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Chats } from 'src/app/models/chats';
 import { Conversacion } from 'src/app/models/conversacion';
+import { ActividadesService } from 'src/app/services/actividades.service';
 import { ListadochatsService } from 'src/app/services/listadochats.service';
 import { UsuarioService } from 'src/app/services/usuario.service';
 @Component({
@@ -12,6 +13,7 @@ export class ChatComponent implements OnInit {
   public miconv:Conversacion=new Conversacion(null,null,null,null,null)
   public miId:number
   public minombre:string
+  public nombrecreador:string
   
   public conversaciones:Conversacion[]=[
     // new Conversacion(1,"Hola que tal"),
@@ -25,21 +27,29 @@ export class ChatComponent implements OnInit {
     // new Conversacion(1,"Tienes alguna duda de la actividad??")
   ]
 
-  constructor(public listadochatsservice :ListadochatsService,public usuarioservice:UsuarioService ) {
+  constructor(public listadochatsservice :ListadochatsService,public usuarioservice:UsuarioService,public actividadservice:ActividadesService ) {
   this.conversaciones= this.listadochatsservice.conversacion
+  console.log(this.listadochatsservice.conversacion);
+  
 this.miId=this.usuarioservice.usuario1.id_usuario
 this.minombre=this.usuarioservice.usuario1.nombre
+// this.nombrecreador=this.actividadservice.actividadesNombreCreador.nombre
   ;
-   }
+   } 
    postMensaje(mensaje:string)
    {
+    let nuevaConversacion = new Conversacion(null,null,mensaje,
+                                            this.usuarioservice.usuario1.id_usuario,
+                                            this.listadochatsservice.miIdchat)
+    console.log(nuevaConversacion);
     
-    this.miconv.id_usuario=this.usuarioservice.usuario1.id_usuario
-    this.miconv.id_chat=this.listadochatsservice.miIdchat
-    this.miconv.mensaje=mensaje
-    this.listadochatsservice.addConversacion(this.miconv).subscribe((data:any)=>{
+    this.conversaciones.push(nuevaConversacion)
+    console.log(this.conversaciones);
+    
+    this.listadochatsservice.addConversacion(nuevaConversacion).subscribe((data:any)=>{
       console.log("conversacion actualizada con "+data)
-      this.conversaciones.push(this.miconv)
+
+      
     })
    }
  
